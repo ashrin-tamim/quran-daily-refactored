88594f9113245178b9a706c13e2c436608fa55f0
ashrin-tamim <ashrintamim.m@gmail.com>
2025-07-14 06:18:50 +0600

    postgres setup complete

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
-import { Controller, Get, Module } from '@nestjs/common';
-
+import { Module } from "@nestjs/common";
+import { UsersModule } from "./users/users.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-    controllers: [],
+  controllers: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      //   url: "postgresql://postgres:825273@localhost:5434/quranDailyDevDB?schema=public",
+      port: 5434,
+      username: "postgres",
+      password: "825273",
+      database: "quranDailyDevDB",
+      entities: [],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
